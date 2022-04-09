bcfc1a66a90e08957243ae56888ca1cc03592a09
bebusl <bebus1998@naver.com>
2022-04-09 22:47:37 +0900

    Feat: 결제/강의 수강페이지 작성

--- src/routes/student/lecture.js
+++ src/routes/student/lecture.js
@@ -1,3 +1,45 @@
-export function Lecture() {
-  return <div>Lecture</div>;
+import AccordianSection from "../../shared/Accordian";
+import { Box, Sidebar, Nav } from "grommet";
+import styled from "styled-components";
+
+const VideoContent = styled.div`
+  min-width: 1200px;
+  width: 100%;
+  min-height: 70vh;
+  background-color: brown;
+`;
+const ContentWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Comment = () => {
+  return (
+    <form>
+      <input
+        style={{
+          border: "1px solid rgba(0,0,0,0.5)",
+          width: "100%",
+          height: "5rem",
+        }}
+      ></input>
+      <button>등록</button>
+    </form>
+  );
+};
+
+export function Lecture({ classname = "TEST CLASS" }) {
+  return (
+    <Box flex direction="row" width={"100%"}>
+      <Sidebar width="250px" header={classname} footer="WAYOUT">
+        <Nav>
+          <AccordianSection openAll={true}></AccordianSection>
+        </Nav>
+      </Sidebar>
+      <ContentWrapper>
+        <VideoContent />
+        <Comment />
+      </ContentWrapper>
+    </Box>
+  );
 }
--- src/routes/student/pay.js
+++ src/routes/student/pay.js
@@ -1,3 +1,43 @@
-export function Pay() {
-  return <div>Pay</div>;
+import { Grid, Box, Heading, Button } from "grommet";
+import Banner from "../../shared/banner";
+
+export function Pay({ total = 25000 }) {
+  return (
+    <Grid
+      rows={["10vh", "70vh"]}
+      columns={["50%", "50%"]}
+      gap="small"
+      areas={[
+        { name: "header", start: [0, 0], end: [1, 0] },
+        { name: "nav", start: [0, 1], end: [0, 1] },
+        { name: "main", start: [1, 1], end: [1, 1] },
+      ]}
+    >
+      <Box gridArea="header">
+        <Heading level="1" size="medium" textAlign="center">
+          Complete Purchase
+        </Heading>
+      </Box>
+      <Box gridArea="nav">
+        <Banner />
+      </Box>
+      <Box gridArea="main">
+        <Heading level="3" size="medium">
+          Payment Options
+        </Heading>
+        <form>
+          <input type="radio" name="payment" value="국내카드 결제" />
+          국내카드 결제
+          <input type="radio" name="payment" value="카카오페이 결제" />
+          카카오페이 결제
+          <input type="radio" name="payment" value="해외카드 결제" />
+          해외카드 결제
+        </form>
+        <Heading level="5" size="medium">
+          최종가격 : ₩ {total}
+        </Heading>
+        <Button primary> Pay Now </Button>
+      </Box>
+    </Grid>
+  );
 }
