b471374002043e7d821c3876a9182da2ea15cd19
bebusl <bebus1998@naver.com>
2022-04-28 12:52:41 +0900

    Chore: 신택스 에러난 것 수정

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,19 +1,25 @@
 import { useState, useCallback } from "react";
 
-export function useInput(includeFile = false) {
-  const [state, setState] = useState({});
+export function useInput(initialValue, includeFile = false) {
+  const [state, setState] = useState(initialValue);
   const handleChange = (e) => {
-    e.preventDefault();
     setState((pre) => ({ ...pre, [e.target.name]: e.target.value }));
   };
-  const getState = useCallback(() => state, [state]);
+  const getState = () => {
+    return state;
+  };
 
   if (includeFile) {
     const handleFile = (e) => {
       e.preventDefault();
       setState((pre) => ({ ...pre, files: e.target.files[0] }));
     };
-    return { handleChange, getState, handleFile };
+    const handleFileCustom = (e) => {
+      const binarydata = e.target.files[0];
+      const base64Data = window.btoa(binarydata);
+      setState((pre) => ({ ...pre, files: base64Data }));
+    };
+    return { handleChange, getState, handleFile, handleFileCustom, state };
   }
-  return { handleChange, getState };
+  return { handleChange, getState, state };
 }
