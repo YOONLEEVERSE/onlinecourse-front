e143dfe367f047b73c832aacfd5f82a8004f4374
bebusl <bebus1998@naver.com>
2022-05-11 12:00:22 +0900

    Feat: 메인화면 실제 데이터 연동

--- src/routes/user/main/index.js
+++ src/routes/user/main/index.js
@@ -1,52 +1,50 @@
 import { ScrollableTabs } from "../../../shared/tab";
 import { Heading, Carousel } from "grommet";
 import CourseCapsule from "./CourseCapsule";
-import { gql, useSubscription } from "@apollo/client";
-
-export const SUBSCRIPTION_TEST = gql`
-  subscription TimeWatch {
-    time {
-      currentTime
-    }
-  }
-`;
+import { gql, useQuery, useApolloClient } from "@apollo/client";
+import { GET_ALL_COURSE } from "../../../graphql/query";
+import { useEffect } from "react";
 
 export function Main({ playtime = 3000 }) {
-  const { data, loading, error } = useSubscription(SUBSCRIPTION_TEST, {
-    onSubscriptionComplete: (data) => {
-      console.log(":::onSubscriptionCOMPLETEResult : ", data);
-    },
-    onSubscriptionData: (data) => {
-      console.log("onSubscriptionData : ", data);
+  const { data } = useQuery(GET_ALL_COURSE, {
+    onCompleted: (data) => {
+      console.log("why ENROLLEMENT ERROR?", data);
     },
   });
-  return (
-    <>
-      {data && data.time && <p>현재시간 {Date(data.time.currentTime)}</p>}
-      <Heading level="3" size="medium">
-        Featured Courses
-      </Heading>
-      <Heading level="4" size="medium">
-        최근 업데이트된 신상 강의
-      </Heading>
-      <Carousel controls={true} play={playtime} height="250px">
-        <CourseCapsule courseName="정환이 바보"></CourseCapsule>
-      </Carousel>
+  const client = useApolloClient();
+  useEffect(() => {
+    const test = client.readQuery({ query: GET_ALL_COURSE });
+    console.log("캐싱 데이터", test);
+  }, [data]);
 
-      <Heading level="3" size="medium">
-        Popular Courses
-      </Heading>
-      <Heading level="4" size="medium">
-        강력 추천하는 베스트 강의
-      </Heading>
-      <ScrollableTabs></ScrollableTabs>
+  if (data) {
+    return (
+      <>
+        <Heading level="3" size="medium">
+          Featured Courses
+        </Heading>
+        <Heading level="4" size="medium">
+          최근 업데이트된 신상 강의
+        </Heading>
+        <Carousel controls={true} play={playtime} height="300px">
+          <CourseCapsule data={data.getAllCourse[0]}></CourseCapsule>
+        </Carousel>
 
-      <Heading level="3" size="medium">
-        Student’s Portfolios
-      </Heading>
-      <Heading level="4" size="medium">
-        수강생 졸업작품
-      </Heading>
-    </>
-  );
+        <Heading level="3" size="medium">
+          Popular Courses
+        </Heading>
+        <Heading level="4" size="medium">
+          강력 추천하는 베스트 강의
+        </Heading>
+        <ScrollableTabs></ScrollableTabs>
+
+        <Heading level="3" size="medium">
+          Student’s Portfolios
+        </Heading>
+        <Heading level="4" size="medium">
+          수강생 졸업작품
+        </Heading>
+      </>
+    );
+  }
 }
